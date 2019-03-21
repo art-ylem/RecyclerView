b8d7a6df932f97ba789f253816b292dad85dbb44
Art-ylem <art-ylem@mail.ru>
2019-03-21 09:05:49 +0500

    create Utils class

--- app/src/main/java/com/example/recyclerview/animals/Utils.java
+++ app/src/main/java/com/example/recyclerview/animals/Utils.java
@@ -1,4 +1,14 @@
 package com.example.recyclerview.animals;
 
 public class Utils {
+    public static String addDote (String s){
+        s += '.';
+        return s;
+    }
+
+    public static int chooseMin(int a, int b){
+        if(a <= b) return a;
+        else return b;
+    }
+
 }
